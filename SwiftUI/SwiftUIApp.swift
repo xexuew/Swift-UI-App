9582482cc958a302692f47c2dd9e292c720fc91c
Jose Torronteras <>
2021-04-05 18:10:59 +0200

    Project creation

---
+++ SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by Jose Torronteras on 05/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
