8ceb73acafa427f8627cc474e16ba6c80e5bc6c6
Jose Torronteras <7280807+xexuew@users.noreply.github.com>
2021-04-05 21:43:06 +0200

    Project creation (#1)
    * Project creation
    
    * Update codemagic.yaml
    
    * Update codemagic.yaml
    
    * Update codemagic.yaml
    
    * Update codemagic.yaml
    
    * Update codemagic.yaml
    
    * Update codemagic.yaml
    
    * Update codemagic.yaml
    
    * Rename project
    
    Co-authored-by: Jose Torronteras <>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Swift-UI-App
+
+[![CodeFactor](https://www.codefactor.io/repository/github/xexuew/swift-ui-app/badge)](https://www.codefactor.io/repository/github/xexuew/swift-ui-app)
---
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by Jose Torronteras on 05/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
