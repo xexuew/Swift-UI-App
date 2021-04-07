8c0f45a3ff4418840a44de28fbf423fcb60b2881
Jose Torronteras <>
2021-04-07 19:35:26 +0200

    Rollbar added, Codecov integration

--- SwiftUIExample/SwiftUIExampleApp.swift
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -6,9 +6,27 @@
 //
 
 import SwiftUI
+import RollbarNotifier
+import RollbarKSCrash
+import RollbarPLCrashReporter
 
 @main
 struct SwiftUIExampleApp: App {
+    @Environment(\.scenePhase) var scenePhase
+    
+    // MARK: - Init
+    init() {
+        let config = RollbarConfig.init()
+        
+        if let rollbarKey = Bundle.main.infoDictionary?["ROLLBAR_KEY"] as? String {
+            config.destination.accessToken = rollbarKey
+            config.destination.environment = "development"
+            
+            // init Rollbar shared instance:
+            Rollbar.initWithConfiguration(config)
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
